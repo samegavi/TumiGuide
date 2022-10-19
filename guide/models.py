2a0732ef3bfc646ea5f8151ddcb2e19e2e5ec1e0
Harriet Oteng <harriet.oteng@gmail.com>
2022-10-19 17:33:43 +0000

    created and activated models for guide app

--- guide/models.py
+++ guide/models.py
@@ -1,3 +1,9 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Place(models.Model):
+    title = models.CharField(max_length = 200)
+
+class Topic(models.Model):
+    title = models.CharField(max_length = 200)
