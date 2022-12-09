414ea91b1a314fa7d129ad6433301cf2367b2a87
Harriet Oteng <harriet.oteng@gmail.com>
2022-12-09 22:53:43 +0000

    Completed CRUD for travel guide

--- guide/admin.py
+++ guide/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Place, Topic, Entry
+
+admin.site.register(Place)
+admin.site.register(Topic)
+admin.site.register(Entry)
--- guide/urls.py
+++ guide/urls.py
@@ -2,9 +2,22 @@ from django.urls import path
 
 from . import views
 
+app_name = "guide"
 urlpatterns = [
     path('', views.index, name='index'),
-    path('places/', views.place, name='place'),
-    path('topics/', views.topic, name='topic'),
-    path('entries/', views.entry, name='entry')
+    path('places/', views.AddPlaceView.as_view(), name='place'),
+    path('places/<int:pk>/', views.PlaceDetailView.as_view(), name='place_detail'),
+    path('places/<int:pk>/edit', views.PlaceUpdateView.as_view(), name='place_update'),
+    path('places/index/', views.PlaceListView.as_view(), name='place_index'),
+    path('places/<int:pk>/delete/', views.PlaceDeleteView.as_view(), name='place_delete'),
+    path('topics/', views.AddTopicView.as_view(), name='topic'),
+    path('topics/<int:pk>/', views.TopicDetailView.as_view(), name='topic_detail'),
+    path('topics/<int:pk>/edit', views.TopicUpdateView.as_view(), name='topic_update'),
+    path('topics/index/', views.TopicListView.as_view(), name='topic_index'),
+    path('topics/<int:pk>/delete/', views.TopicDeleteView.as_view(), name='topic_delete'),
+    path('entries/', views.AddEntryView.as_view(), name='entry'),
+    path('entries/<int:pk>/', views.EntryDetailView.as_view(), name='entry_detail'),
+    path('entries/<int:pk>/edit', views.EntryUpdateView.as_view(), name='entry_update'),
+    path('entries/index/', views.EntryListView.as_view(), name='entry_index'),
+    path('entries/<int:pk>/delete/', views.EntryDeleteView.as_view(), name='entry_delete'),
 ]
